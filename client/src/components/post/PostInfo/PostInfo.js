97429696bf924236f7cfdb8805a5eb68bd6b826e
kidow <wcgo2ling@hanmail.net>
2018-11-01 14:05:30 +0900

    PostInfo set

---
+++ client/src/components/post/PostInfo/PostInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './PostInfo.scss'
+
+const PostInfo = () => {
+  return (
+    <div className='post-info'>
+      <div className='info'>
+        <h1>타이틀</h1>
+        <div className='tags'>
+          <a>#태그</a>
+          <a>#태그</a>
+          <a>#태그</a>
+        </div>
+        <div className='date'>Oct 29, 2017</div>
+      </div>
+    </div>
+  );
+};
+
+export default PostInfo;
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PageTemplate from 'components/common/PageTemplate'
+import PageInfo from 'components/post/PostInfo'
 
 const PostPage = () => {
   return (
-    <div>
-      PostPage
-    </div>
+    <PageTemplate>
+      <PageInfo />
+    </PageTemplate>
   );
 };
 
