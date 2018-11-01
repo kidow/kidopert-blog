9c9bb9a5bd35eb52d8082bda81c5ef06f6b35578
kidow <wcgo2ling@hanmail.net>
2018-11-01 13:27:41 +0900

    Footer Component set

---
+++ client/src/components/common/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <Link to='/' className='brand'>reactblog</Link>
+      <div className='admin-login'>관리자 로그인</div>
+    </footer>
+  );
+};
+
+export default Footer;
